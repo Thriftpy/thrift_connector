407386cf9406e928afce499e5e6ba6a24edd0511
WooParadog <guohaochuan@gmail.com>
2014-11-10 17:44:49 +0800

    bump to 0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 # package meta info
 NAME = "thrift_connector"
-VERSION = "0.4"
+VERSION = "0.5"
 DESCRIPTION = "Lib to connect to thrift services with pools"
 AUTHOR = "Haochuan Guo"
 AUTHOR_EMAIL = "guohaochuan@gmail.com"
