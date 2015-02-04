7eef9366b2e0e4d945c3b4f0c6ed0914a98afe1e
Marconi Moreto Jr <git@marconijr.com>
2015-02-04 11:22:12 +0800

    add ThriftConnection, ThriftClientMux, BaseClientPoolMux and ClientPoolMux

--- thrift_connector/connection_pool.py
+++ thrift_connector/connection_pool.py
@@ -10,20 +10,50 @@ import time
 logger = logging.getLogger(__name__)
 
 
+class ThriftConnection(object):
+    def __init__(self, protocol, transport, pool_generation, keepalive):
+        self.protocol = protocol
+        self.transport = transport
+        self.pool_generation = pool_generation
+        self.alive_until = datetime.datetime.now() + \
+            datetime.timedelta(seconds=keepalive) if keepalive else None
+
+    def is_expired(self):
+        return self.alive_until and datetime.datetime.now() > self.alive_until
+
+    def test_connection(self):
+        return False if self.is_expired or not self.transport.isOpen() else True
+
+    def close(self):
+        try:
+            self.transport.close()
+        except Exception as e:
+            logger.warn("Connection close failed: %r" % e)
+
+
 class ThriftBaseClient(object):
     def __init__(self, host, port, transport, protocol, service,
                  keepalive=None, pool_generation=0, tracking=False,
-                 tracker_factory=None):
+                 tracker_factory=None, connection=None):
         self.host = host
         self.port = port
         self.transport = transport
         self.protocol = protocol
         self.service = service
-        self.alive_until = datetime.datetime.now() + \
-            datetime.timedelta(seconds=keepalive) if keepalive else None
         self.pool_generation = pool_generation
         self.tracking = tracking
         self.tracker_factory = tracker_factory
+
+        if not connection:
+            self._connection = ThriftConnection(
+                self.protocol,
+                self.transport,
+                self.pool_generation,
+                keepalive)
+        else:
+            self._connection = connection
+            self.transport = connection.transport
+            self.protocol = connection.protocol
 
         self.client = self.get_tclient(service, protocol)
 
@@ -37,22 +67,16 @@ class ThriftBaseClient(object):
         return getattr(self.client, name)
 
     def close(self):
-        try:
-            self.transport.close()
-        except Exception as e:
-            logger.warn("Connction close failed: %r" % e)
+        self._connection.close()
 
     def is_expired(self):
-        return self.alive_until and datetime.datetime.now() > self.alive_until
+        self._connection.is_expired()
+
+    def get_connection(self):
+        return self._connection
 
     def test_connection(self):
-        if self.is_expired():
-            return False
-        try:
-            self.ping()
-            return True
-        except:
-            return False
+        self._connection.test_connection()
 
     @classmethod
     def connect(cls, service, host, port, timeout=30, keepalive=None,
@@ -109,6 +133,43 @@ class ThriftClient(ThriftBaseClient):
     def get_socket_factory(self):
         from thrift.transport import TSocket
         return TSocket.TSocket
+
+
+class ThriftClientMux(ThriftClient):
+    @classmethod
+    def get_protoco_factory(self):
+        from thrift.protocol import TBinaryProtocol
+        return TBinaryProtocol.TBinaryProtocolAccelerated
+
+    @classmethod
+    def get_transport_factory(self):
+        from thrift.transport import TTransport
+        return TTransport.TFramedTransport
+
+    @classmethod
+    def connect(cls, svc_name, service, host, port, timeout=30, keepalive=None,
+                pool_generation=0, tracking=False, tracker_factory=None):
+        SOCKET = cls.get_socket_factory()(host, port)
+        PROTO_FACTORY = cls.get_protoco_factory()
+        TRANS_FACTORY = cls.get_transport_factory()
+
+        transport = TRANS_FACTORY(SOCKET)
+        protocol = PROTO_FACTORY(transport)
+
+        from thrift.protocol import TMultiplexedProtocol
+        protocol = TMultiplexedProtocol.TMultiplexedProtocol(protocol, svc_name)
+
+        transport.open()
+
+        return cls(host=host,
+                   port=port,
+                   transport=transport,
+                   protocol=protocol,
+                   service=service,
+                   keepalive=keepalive,
+                   pool_generation=pool_generation,
+                   tracking=tracking,
+                   tracker_factory=tracker_factory)
 
 
 class ThriftPyClient(ThriftBaseClient):
@@ -175,7 +236,7 @@ class ThriftPyCyClient(ThriftBaseClient):
 
 class BaseClientPool(object):
     def __init__(self, service, timeout=30, name=None,
-                 raise_empty=False, max_conn=30, connction_class=ThriftClient,
+                 raise_empty=False, max_conn=30, connection_class=ThriftClient,
                  keepalive=None, tracking=False, tracker_factory=None):
         self.service = service
         self.timeout = timeout
@@ -183,7 +244,7 @@ class BaseClientPool(object):
         self.connections = set()
         self.raise_empty = raise_empty
         self.max_conn = max_conn
-        self.connction_class = connction_class
+        self.connection_class = connection_class
         self.keepalive = keepalive
         self.generation = 0
         self.tracking = tracking
@@ -233,7 +294,7 @@ class BaseClientPool(object):
 
     def produce_client(self):
         host, port = self.yield_server()
-        return self.connction_class.connect(
+        return self.connection_class.connect(
             self.service,
             host,
             port,
@@ -275,7 +336,7 @@ class BaseClientPool(object):
 
 class ClientPool(BaseClientPool):
     def __init__(self, service, host, port, timeout=30, name=None,
-                 raise_empty=False, max_conn=30, connction_class=ThriftClient,
+                 raise_empty=False, max_conn=30, connection_class=ThriftClient,
                  keepalive=None, tracking=False, tracker_factory=None):
         super(ClientPool, self).__init__(
             service=service,
@@ -283,7 +344,7 @@ class ClientPool(BaseClientPool):
             name=name,
             raise_empty=raise_empty,
             max_conn=max_conn,
-            connction_class=connction_class,
+            connection_class=connection_class,
             keepalive=keepalive,
             tracking=tracking,
             tracker_factory=tracker_factory,
@@ -300,10 +361,152 @@ class ClientPool(BaseClientPool):
         return self.host, self.port
 
 
+class BaseClientPoolMux(BaseClientPool):
+    def __init__(self, services, timeout=30, raise_empty=False, max_conn=30,
+                 connection_class=ThriftClient, keepalive=None, tracking=False,
+                 tracker_factory=None):
+
+        self.services = services
+        self.timeout = timeout
+        self.connections = set()
+        self.raise_empty = raise_empty
+        self.max_conn = max_conn
+        self.connection_class = connection_class
+        self.keepalive = keepalive
+        self.generation = 0
+        self.tracking = tracking
+        self.tracker_factory = tracker_factory
+
+    def keys(self):
+        return [(svc_name, svc.__name__) for svc_name, svc in self.services.items()]
+
+    def __repr__(self):
+        return "<%s service=%r>" % (
+            self.__class__.__name__,
+            self.keys()
+            )
+
+    def pool_size(self):
+        return len(self.connections)
+
+    def clear(self):
+        old_connections = self.connections
+        self.connections = set()
+        self.generation += 1
+
+        for c in old_connections:
+            c.close()
+
+    def get_connection_from_pool(self):
+        if not self.connections:
+            if self.raise_empty:
+                raise self.Empty
+            return
+
+        connection = self.connections.pop()
+        if connection.test_connection():  # make sure old connection is usable
+            return connection
+        else:
+            connection.close()
+
+    def put_back_connection(self, connection):
+        if (self.max_conn > 0 and len(self.connections) < self.max_conn
+            and connection.pool_generation == self.generation):
+
+            self.connections.add(connection)
+            return True
+
+        connection.close()
+        return False
+
+    def produce_client(self, svc_name):
+        host, port = self.yield_server()
+        return self.connection_class.connect(
+            svc_name,
+            self.services.get(svc_name),
+            host,
+            port,
+            self.timeout,
+            keepalive=self.keepalive,
+            pool_generation=self.generation,
+            tracking=self.tracking,
+            tracker_factory=self.tracker_factory)
+
+    def get_client(self, svc_name):
+        connection = self.get_connection_from_pool()
+        if not connection:
+            return self.produce_client(svc_name)
+
+        host, port = self.yield_server()
+        return self.connection_class(
+            host=host,
+            port=port,
+            protocol=None,
+            transport=None,
+            service=self.services.get(svc_name),
+            keepalive=self.keepalive,
+            pool_generation=self.generation,
+            tracking=self.tracking,
+            tracker_factory=self.tracker_factory,
+            connection=connection)
+
+    def __getattr__(self, name):
+        def call(svc_name, *args, **kwds):
+            client = self.get_client(svc_name)
+            api = getattr(client, name, None)
+            try:
+                if api and callable(api):
+                    return api(*args, **kwds)
+                raise AttributeError("%s not found in %s" % (name, client))
+            finally:
+                self.put_back_connection(client.get_connection())
+        return call
+
+    @contextlib.contextmanager
+    def connection_ctx(self, svc_name):
+        client = self.get_client(svc_name)
+        try:
+            yield client
+            self.put_back_connection(client.get_connection())
+        except client.TTransportException:
+            client.close()
+            raise
+        except Exception:
+            self.put_back_connection(client.get_connection())
+            raise
+
+
+class ClientPoolMux(BaseClientPoolMux):
+    def __init__(self, services, host, port, timeout=30, raise_empty=False,
+                 max_conn=30, connection_class=ThriftClient, keepalive=None,
+                 tracking=False, tracker_factory=None):
+
+        super(ClientPoolMux, self).__init__(
+            services=services,
+            timeout=timeout,
+            raise_empty=raise_empty,
+            max_conn=max_conn,
+            connection_class=connection_class,
+            keepalive=keepalive,
+            tracking=tracking,
+            tracker_factory=tracker_factory)
+
+        self.host = host
+        self.port = port
+
+    def set_servers(self, server_info):
+        host, port = server_info
+        self.host = host
+        self.port = port
+
+    def yield_server(self):
+        return self.host, self.port
+
+
 class HeartbeatClientPool(ClientPool):
 
     def __init__(self, service, host, port, timeout=30, name=None,
-                 raise_empty=False, max_conn=30, connction_class=ThriftClient,
+                 raise_empty=False, max_conn=30, connection_class=ThriftClient,
                  keepalive=None, tracking=False, tracker_factory=None):
         super(HeartbeatClientPool, self).__init__(
             service=service,
@@ -313,7 +516,7 @@ class HeartbeatClientPool(ClientPool):
             name=name,
             raise_empty=raise_empty,
             max_conn=max_conn,
-            connction_class=connction_class,
+            connection_class=connection_class,
             keepalive=keepalive,
             tracking=tracking,
             tracker_factory=tracker_factory
@@ -374,7 +577,7 @@ class HeartbeatClientPool(ClientPool):
 
 class MultiServerClientBase(ClientPool):
     def __init__(self, service, servers, timeout=30, name=None,
-                 raise_empty=False, max_conn=30, connction_class=ThriftClient,
+                 raise_empty=False, max_conn=30, connection_class=ThriftClient,
                  keepalive=None, tracking=False, tracker_factory=None):
         super(MultiServerClientBase, self).__init__(
             service=service,
@@ -382,7 +585,7 @@ class MultiServerClientBase(ClientPool):
             name=name,
             raise_empty=raise_empty,
             max_conn=max_conn,
-            connction_class=connction_class,
+            connection_class=connection_class,
             keepalive=keepalive,
             tracking=tracking,
             tracker_factory=None,
